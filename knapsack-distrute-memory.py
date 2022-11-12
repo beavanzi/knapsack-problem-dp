303a92b1d87f7b2731070492d45c7fd12f919421
luizokada <luizfernandookada@gmail.com>
2022-11-12 00:08:51 -0300

    Changing tests

--- knapsack-distrute-memory.py
+++ knapsack-distrute-memory.py
@@ -1,56 +1,13 @@
 from time import sleep, perf_counter
 from threading import Thread
 
-wt = [382745,
-799601,
-909247,
-729069,
-467902,
- 44328,
- 34610,
-698150,
-823460,
-903959,
-853665,
-551830,
-610856,
-670702,
-488960,
-951111,
-323046,
-446298,
-931161,
- 31385,
-496951,
-264724,
-224916,
-169684,]
+ 
+wt = [23,31,29,44,53,38,63,85,89,82]
 
-val = [825594,
-1677009,
-1676628,
-1523970,
- 943972,
-  97426,
-  69666,
-1296457,
-1679693,
-1902996,
-1844992,
-1049289,
-1252836,
-1319836,
- 953277,
-2067538,
- 675367,
- 853655,
-1826027,
-  65731,
- 901489,
- 577243,
- 466257,
- 369261,]
-W = 6404180
+val = [92,57,49,68,60,43,67,84,87,72]
+
+W = 165
+
 n = len(val)
 
 K = [[0 for x in range(W + 1)] for x in range(n + 1)]
@@ -74,8 +31,8 @@ def loopKnap(W, wt, val, init,i):
             setK(i,w,0)
         elif wt[i-1] <= w:
             setK(i,w,max(val[i-1]
-                        + K[i-1][w-wt[i-1]], 
-                            K[i-1][w]))
+                        + getK()[i-1][w-wt[i-1]], 
+                            getK()[i-1][w]))
         else:
             setK(i,w,getK()[i-1][w])
     return
@@ -84,17 +41,13 @@ def knapSack(W, wt, val, n):
     
     start_time = perf_counter()
 
-    for i in range(n + 1):
-       
-        t1 = Thread(target=loopKnap,args=(W//2, wt, val,0, i,K))
-        t2 = Thread(target=loopKnap,args=(W, wt, val,W//2, i,K))
- 
+    for i in range(n + 1):       
+        t1 = Thread(target=loopKnap,args=(W//2, wt, val,0, i))
+        t2 = Thread(target=loopKnap,args=(W, wt, val,W//2, i))
         t1.start()
         t2.start()
-
         t1.join()
         t2.join()
-        
     end_time = perf_counter()
     print(f'Tempo para completar {end_time- start_time} ms.')
     
@@ -102,4 +55,7 @@ def knapSack(W, wt, val, n):
  
  
 
+
 print(knapSack(W, wt, val, n))
+
+
--- knapsack-parallel.py
+++ knapsack-parallel.py
@@ -11,26 +11,17 @@ def loopKnap(W, wt, val, init,i,K):
                           + K[i-1][w-wt[i-1]], 
                               K[i-1][w])
             else:
-                K[i][w] = K[i-1][w]
-              
+                K[i][w] = K[i-1][w]              
     return
 
 def knapSack(W, wt, val, n):
     K = [[0 for x in range(W + 1)] for x in range(n + 1)]
-    
-    
     start_time = perf_counter()
-
     for i in range(n + 1):
-       
         t1 = Thread(target=loopKnap,args=(W//2, wt, val,0, i,K))
         t2 = Thread(target=loopKnap,args=(W, wt, val,W//2, i,K))
-        
-        # start the threads
         t1.start()
         t2.start()
-
-        # wait for the threads to complete
         t1.join()
         t2.join()
     end_time = perf_counter()
@@ -40,61 +31,12 @@ def knapSack(W, wt, val, n):
     return K[n][W]
  
  
-wt = [382745,
-799601,
-909247,
-729069,
-467902,
- 44328,
- 34610,
-698150,
-823460,
-903959,
-853665,
-551830,
-610856,
-670702,
-488960,
-951111,
-323046,
-446298,
-931161,
- 31385,
-496951,
-264724,
-224916,
-169684,]
+wt = [23,31,29,44,53,38,63,85,89,82]
 
-val = [825594,
-1677009,
-1676628,
-1523970,
- 943972,
-  97426,
-  69666,
-1296457,
-1679693,
-1902996,
-1844992,
-1049289,
-1252836,
-1319836,
- 953277,
-2067538,
- 675367,
- 853655,
-1826027,
-  65731,
- 901489,
- 577243,
- 466257,
- 369261,]
-W = 6404180
+val = [92,57,49,68,60,43,67,84,87,72]
+
+W = 165
+
 n = len(val)
-# Driver code
-start_time = perf_counter()
 
 print(knapSack(W, wt, val, n))
-
-end_time = perf_counter()
-print(f'It took {end_time-start_time} ms to complete.')
